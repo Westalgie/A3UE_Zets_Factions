fc028016c486e206abfafad3f821997272a5d152
Westalgie <lexa.zaytsev@gmail.com>
2023-10-08 18:48:25 +0300

    blacklist not working?

---
+++ a3au_inTeam_factions/addons/ultimate/config.cpp
@@ -0,0 +1,33 @@
+#include "script_component.hpp"
+
+class CfgPatches 
+{
+    class ADDON 
+    {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3A_Events"};
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgFunctions.hpp"
+
+class A3U
+{
+    #include "config\cfgForbiddenItems.hpp"
+    #include "config\trader\cfgTraderMods.hpp"
+};
+
+class Extended_PreInit_EventHandlers 
+{
+    class A3U_init
+	{
+        init = "call A3U_fnc_init";
+    };
+};
---
+++ a3au_inTeam_factions/addons/ultimate/config/trader/cfgTraderMods.hpp
@@ -0,0 +1,14 @@
+class traderMods
+{
+    class base
+    {
+        addons[] = {};
+        prefix = "";
+    };
+
+    class inTeam_PMC : base
+    {
+        addons[] = {"LTF_PMC"};
+        prefix = "pmc";
+    };
+};
