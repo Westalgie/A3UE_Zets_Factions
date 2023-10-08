2c692f5dd006f98febc8b4bc4d498533b96a5ed4
Westalgie <lexa.zaytsev@gmail.com>
2023-10-08 14:45:24 +0300

    classnames test

--- a3au_inTeam_factions/addons/logistics/Nodes/inTeamNodes.hpp
+++ a3au_inTeam_factions/addons/logistics/Nodes/inTeamNodes.hpp
@@ -1,4 +1,55 @@
-class rds_rds_a2port_civ_Lada_Lada : TRIPLES(ADDON,Nodes,Base)
+class RDS_Lada_Civ_01 : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.25,0.11};
+            seats[] = {};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.25,0.11};
+            seats[] = {};
+        };
+    };
+};
+
+class RDS_Lada_Civ_02 : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.25,0.11};
+            seats[] = {};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.25,0.11};
+            seats[] = {};
+        };
+    };
+};
+
+class RDS_Lada_Civ_03 : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.25,0.11};
+            seats[] = {};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.25,0.11};
+            seats[] = {};
+        };
+    };
+};
+
+class RDS_Lada_Civ_04 : TRIPLES(ADDON,Nodes,Base)
 {
     class Nodes
     {
