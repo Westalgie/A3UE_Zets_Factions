76944e06c0a50f0d491a232e8cf695e0446ece52
Westalgie <lexa.zaytsev@gmail.com>
2023-10-14 17:01:56 +0300

    Takistan factions, loot sorting logic, stringtable v1

--- a3au_inTeam_factions/addons/templates/Templates/Templates.hpp
+++ a3au_inTeam_factions/addons/templates/Templates/Templates.hpp
@@ -1,34 +1,36 @@
 class Templates {
     class RHS_Base; //import RHS_Base from a3au to use with defining a new RHS based template
 
-    class inTeam_Base : RHS_Base
+    class InTeam_Base : RHS_Base
     {
-        requiredAddons[] = {"rhsgref_main", "rhssaf_c_vehicles", "rhs_c_tanks", "RHS_US_A2Port_Armor"};
-        basepath = QPATHTOFOLDER(Templates\inTeam); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
+        requiredAddons[] = {"rhsgref_main", "rhssaf_c_vehicles", "rhs_c_tanks", "RHS_US_A2Port_Armor", "UK3CB_Factions_Vehicles_SUV"};
+        basepath = QPATHTOFOLDER(Templates\InTeam); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
         logo = QPATHTOFOLDER(data\inTeam_logo_ca.paa);
-        priority = 30;
+        priority = 60;
         equipFlags[] = {"specialRHS"};
     };
 
-    // class inTeam_Wagner_PMC : inTeam_Base
-    // {
-    //     requiredAddons[] = {"LTF_PMC"};
-    //     side = "Inv"; //the side the faction defaults to, one of the following: Inv, Occ, Reb, Civ
-    //     flagTexture = QPATHTOFOLDER(data\inTeam_wagner_pmc_flag_co.paa); //path to an icon to be displayed in the selector
-    //     name = "Wagner PMC"; //the name shown in the selector
-    //     file = "inTeam_AI_Wagner_PMC"; //the template file name
-    //     description = "Оооо бля, а мы и не думали...";
-    // };
+    class InTeam_NGTR : InTeam_Base
+    {
+        side = "Occ";
+        flagTexture = "\UK3CB_Factions\addons\UK3CB_Factions_TKA\Flag\tka_flag_co.paa";
+        name = $STR_InTeam_NGTR_name;
+        file = "InTeam_AI_NGTR";
+        maps[] = {"takistan"};
+        climate[] = {"arid"};
+        description = $STR_InTeam_NGTR_desription;
+    };
 
-    // class inTeam_UAF : inTeam_Base
-    // {
-    //     requiredAddons[] = {"afou_main"};
-    //     side = "Occ"; //the side the faction defaults to, one of the following: Inv, Occ, Reb, Civ
-    //     flagTexture = QPATHTOFOLDER(data\inTeam_uaf_flag_co.paa); //path to an icon to be displayed in the selector
-    //     name = "UAF"; //the name shown in the selector
-    //     file = "inTeam_AI_UAF"; //the template file name
-    //     description = "Ukrain armed forces";
-    // };
+    class InTeam_PLAT : InTeam_Base
+    {
+        side = "Inv";
+        flagTexture = QPATHTOFOLDER(data\InTeam_flag_PLAT_co.paa);
+        name = $STR_InTeam_PLAT_name;
+        file = "InTeam_AI_PLAT";
+        maps[] = {"takistan"};
+        climate[] = {"arid"};
+        description = $STR_InTeam_PLAT_description;
+    };
 
     // class inTeam_Civ : inTeam_Base
     // {
@@ -39,13 +41,13 @@ class Templates {
     //     file = "inTeam_Civ";
     // };
 
-    // class inTeam_NAPA : inTeam_Base
-    // {
-    //     requiredAddons[] = {"rds_A2_Civilians"};
-    //     side = "Reb";
-    //     flagTexture = "\rhsgref\addons\rhsgref_main\data\Flags\flag_NAPA_co.paa";
-    //     name = "inTeam NAPA";
-    //     file = "inTeam_Reb_NAPA";
-    //     description = $STR_A3AP_setupFactionsTab_napa_3cbf;
-    // };
+    class InTeam_TCM : InTeam_Base
+    {
+        side = "Reb";
+        flagTexture = QPATHTOFOLDER(data\InTeam_flag_TCM_co.paa);
+        name = $STR_InTeam_TCM_name;
+        file = "InTeam_Reb_TCM";
+        maps[] = {"takistan"};
+        description = $STR_InTeam_TCM_description;
+    };
 };
