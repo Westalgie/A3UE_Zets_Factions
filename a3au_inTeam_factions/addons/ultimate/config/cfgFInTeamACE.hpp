7b8ba6a5e1b2ec80914b46755c429ad6bbbfff31
Westalgie <lexa.zaytsev@gmail.com>
2023-10-21 22:08:12 +0300

    forbidden items new standard

--- a3au_inTeam_factions/addons/ultimate/config/cfgFInTeamACE.hpp
+++ a3au_inTeam_factions/addons/ultimate/config/cfgFInTeamACE.hpp
@@ -1,3 +1,7 @@
+class ACE_InTeam_limited_base : forbidden_limited_base {
+	addons[] = {"ace_main"};
+};
+
 // ace mine detector
 class ACE_VMM3: forbidden_limited_base {};
 class ACE_VMH3: forbidden_limited_base {};
--- a3au_inTeam_factions/addons/ultimate/config/cfgForbiddenItems.hpp
+++ a3au_inTeam_factions/addons/ultimate/config/cfgForbiddenItems.hpp
@@ -1,18 +1,9 @@
 class forbiddenItems
 {
-    class forbidden_unlimited_base
-    {
-        unlimited = 1;
-        appearInCrates = 0;
-    };
-    class forbidden_limited_base : forbidden_unlimited_base
-    {
-        unlimited = 0;
-    };
+    class forbidden_limited_base;
+    class forbidden_unlimited_base;
 
     #include "cfgFInTeamACE.hpp"
-    // #include "cfgFInTeamPMC.hpp"
-    // #include "cfgFInTeamRDS.hpp"
     #include "cfgFInTeamRHS.hpp"
     #include "cfgFInTeam3CB.hpp"
 };
