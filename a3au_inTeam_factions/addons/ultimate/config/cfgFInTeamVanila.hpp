089f8836e6213b0d9447e831279a2cc20cb632e1
Westalgie <lexa.zaytsev@gmail.com>
2023-10-22 00:26:35 +0300

    blacklist tsing

---
+++ a3au_inTeam_factions/addons/ultimate/config/cfgFInTeamVanila.hpp
@@ -0,0 +1,3 @@
+class Vanilla_unlimited_base : forbidden_unlimited_base {
+	addons[] = {"A3_Weapons_F"};
+};
--- a3au_inTeam_factions/addons/ultimate/config/cfgForbiddenItems.hpp
+++ a3au_inTeam_factions/addons/ultimate/config/cfgForbiddenItems.hpp
@@ -3,8 +3,11 @@ class forbiddenItems
     class forbidden_limited_base;
     class forbidden_unlimited_base;
 
+    class A3AP_SelfReviveKit : forbidden_limited_base;
+
     #include "cfgFInTeamACE.hpp"
     #include "cfgFInTeamRHS.hpp"
     #include "cfgFInTeam3CB.hpp"
-    #include "cfgFInTeamTFR"
+    #include "cfgFInTeamTFAR.hpp"
 };
+
