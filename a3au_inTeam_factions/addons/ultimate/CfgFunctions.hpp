aade947c3169b4363a992e865b23423b18da0472
Westalgie <lexa.zaytsev@gmail.com>
2023-10-22 09:43:56 +0300

    typo, cba settings

--- a3au_inTeam_factions/addons/ultimate/CfgFunctions.hpp
+++ a3au_inTeam_factions/addons/ultimate/CfgFunctions.hpp
@@ -5,15 +5,15 @@ class CfgFunctions
 		class Ammunitions
 		{
 			class fillLootCrate {
-				file = QPATHTOFOLDER(functions\Ammunitions\fn_fillLootCrate.sqf)
+				file = QPATHTOFOLDER(functions\Ammunitions\fn_fillLootCrate.sqf);
 			};
 			class loot {
-				file = QPATHTOFOLDER(functions\Ammunitions\fn_loot.sqf)
+				file = QPATHTOFOLDER(functions\Ammunitions\fn_loot.sqf);
 			};
 		};
 		class Inventory
 		{
-			file = QPATHTOFOLDER(functions\Inventory)
+			file = QPATHTOFOLDER(functions\Inventory);
 			class compatibleMagazinesWithExceptions {};
 		};
 	};
--- a3au_inTeam_factions/addons/ultimate/config/cfgForbiddenItems.hpp
+++ a3au_inTeam_factions/addons/ultimate/config/cfgForbiddenItems.hpp
@@ -3,8 +3,8 @@ class forbiddenItems
     class forbidden_limited_base;
     class forbidden_unlimited_base;
 
-    class A3AP_SelfReviveKit : forbidden_limited_base;
-
+    class A3AP_SelfReviveKit : forbidden_limited_base {};
+ 
     #include "cfgFInTeamACE.hpp"
     #include "cfgFInTeamRHS.hpp"
     #include "cfgFInTeam3CB.hpp"
